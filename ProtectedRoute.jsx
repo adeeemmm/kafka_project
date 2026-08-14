a68b6e16c3c53818316458b8e039dec122f6fb28
Adem Hadj Ltaif <hadjltaifadem@gmail.com>
2026-08-14 10:19:08 +0200

    Delete ProtectedRoute.jsx

--- ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { getToken } from "./services/api";
-
-function ProtectedRoute({ children }) {
-  const token = getToken();
-  return token ? children : <Navigate to="/login" replace />;
-}
-
-export default ProtectedRoute;
